f2f5cf857f4f6db628eaf465f86b78c751a5ed48
Ethan Seiber <ethcseib@ut.utm.edu>
2019-01-15 14:56:19 -0600

    Changed Hello world to a guessing game

--- git project/Program.cs
+++ git project/Program.cs
@@ -10,7 +10,42 @@ namespace git_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 1;
+            Random rand = new Random();
+
+            Console.WriteLine("I am thinking of a number between 0- 100");
+
+            int CorrectAnswer = rand.Next()%100;
+
+            bool val = true;
+            
+
+            while (val) {
+
+                string UserAnswer = Console.ReadLine();
+
+                int x = Convert.ToInt32(UserAnswer);
+ 
+                if (x > CorrectAnswer)
+                {
+                    Console.WriteLine("Answer was too high\n");
+                    count++;
+                }
+
+                else if (x < CorrectAnswer)
+                {
+                    Console.WriteLine("Answer was too low\n");
+                    count++;
+                }
+
+                else
+                {
+                    Console.WriteLine("Correct! Your total tries were " + Convert.ToString(count) + "\n");
+                    val = false;
+                }
+                
+            }
+
             Console.ReadKey();
         }
     }
